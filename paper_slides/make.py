f0b9724ac7974d2bc3640e8bce25b547c4c366c7
Jacob Conway <jcconway@stanford.edu>
2024-07-29 12:48:00 -0500

    #1 Commented out code/slides.tex

--- paper_slides/make.py
+++ paper_slides/make.py
@@ -48,7 +48,7 @@ gs.tablefill(template = 'code/tables.tex',
 ### RUN SCRIPTS
 gs.run_latex(PATHS, program = 'code/paper.tex')
 gs.run_latex(PATHS, program = 'code/online_appendix.tex')
-gs.run_latex(PATHS, program = 'code/slides.tex')
+#gs.run_latex(PATHS, program = 'code/slides.tex')
 
 ### LOG OUTPUTS
 gs.log_files_in_output(PATHS)
